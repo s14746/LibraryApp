bb07d227672f0db32ad669ef51a2524b26905d23
Marlena Franckiewicz <s14746@pjwstk.edu.pl>
2018-10-18 20:06:27 +0200

    Create class Book and Author

---
+++ src/main/java/domain/Book.java
@@ -0,0 +1,59 @@
+package domain;
+
+public class Book {
+
+    private int id;
+    private String title;
+    private int authorId;
+    private int yearOfPublishment;
+    private String publishingHouse;
+    private boolean availability;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(int authorId) {
+        this.authorId = authorId;
+    }
+
+    public int getYearOfPublishment() {
+        return yearOfPublishment;
+    }
+
+    public void setYearOfPublishment(int yearOfPublishment) {
+        this.yearOfPublishment = yearOfPublishment;
+    }
+
+    public String getPublishingHouse() {
+        return publishingHouse;
+    }
+
+    public void setPublishingHouse(String publishingHouse) {
+        this.publishingHouse = publishingHouse;
+    }
+
+    public boolean isAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(boolean availability) {
+        this.availability = availability;
+    }
+}
