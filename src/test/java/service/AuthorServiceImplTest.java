29ad9dc525c19281423bb6afe3fe026c75edeb01
Marlena Franckiewicz <s14746@pjwstk.edu.pl>
2018-10-19 21:09:49 +0200

    Add method readNotFoundTest and readFoundTest.

--- src/test/java/service/AuthorServiceImplTest.java
+++ src/test/java/service/AuthorServiceImplTest.java
@@ -62,4 +62,49 @@ public class AuthorServiceImplTest {
         Assert.assertNotNull(authors);
         Assert.assertEquals(3, authors.size());
     }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void readNotFoundTest() {
+
+        // given
+        Author author1 = new Author();
+        author1.setId(1);
+        author1.setName("Adam");
+        author1.setSurname("Mickiewicz");
+        authorService.create(author1);
+
+        Author author2 = new Author();
+        author2.setId(2);
+        author2.setName("Henryk");
+        author2.setSurname("Sienkiewicz");
+        authorService.create(author2);
+
+        // when
+        authorService.read(3);
+    }
+
+    @Test
+    public void readFoundTest(){
+
+        //given
+        Author author1 = new Author();
+        author1.setId(1);
+        author1.setName("Adam");
+        author1.setSurname("Mickiewicz");
+        authorService.create(author1);
+
+        Author author2 = new Author();
+        author2.setId(2);
+        author2.setName("Henryk");
+        author2.setSurname("Sienkiewicz");
+        authorService.create(author2);
+
+        // when
+        authorService.read(2);
+
+        // then
+        Assert.assertEquals(2,2);
+
+    }
+
 }
